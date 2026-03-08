06d486853a7a4d0571b53d82957111b110eceebe
0xMUEL <45379550+0xMUEL@users.noreply.github.com>
2026-03-08 23:18:33 +0000

    Deploying to gh-pages from @ 0xMUEL/MolaMola@bae514a4537f19517f9239ae16c07d3ff21348ef 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://0xMUEL.github.io/mola-mola-site/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://0xMUEL.github.io/MolaMola/#contact">contact us</a>.'
               }
             ]
           }
